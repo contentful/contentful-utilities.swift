4a6f0e64c041a7a9d7ac0d85eb2f0d10f7404d5f
JP Wright <jp@contentful.com>
2018-04-25 18:19:06 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["ContentfulSyncSerializer"])
     ],
     dependencies: [
-      .package(url: "https://github.com/contentful/contentful-persistence.swift", .upToNextMinor(from: "0.10.0")),
+      .package(url: "https://github.com/contentful/contentful-persistence.swift", .upToNextMinor(from: "0.11.0")),
       .package(url: "https://github.com/contentful/contentful.swift", .upToNextMinor(from: "2.0.0")),
       .package(url: "https://github.com/jensravens/Interstellar", .upToNextMinor(from: "2.1.0")),
       .package(url: "https://github.com/kylef/Commander", .upToNextMinor(from: "0.8.0")),
